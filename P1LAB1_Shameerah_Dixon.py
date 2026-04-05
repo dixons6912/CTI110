c274c889cbc06dd36b296e177693df510f8c7dab
dixons6912 <dixons6912@student.faytechcc.edu>
2026-04-05 15:34:37 -0400

    Add files via upload

---
+++ P1LAB1_Shameerah_Dixon.py
@@ -0,0 +1,13 @@
+# Shameerah Dixon
+# April 5, 2026
+# P1LAB1
+# A brief description of the project: get input from the user and display output
+
+# Get the user's first name
+first_name = input("Enter your first name: ")
+
+# Get the user's last name
+last_name = input("Enter your last name: ")
+
+# Display the welcome message
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
