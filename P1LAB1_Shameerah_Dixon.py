64a8c804e7f7e5028609c02d22b9b8983abf2f64
dixons6912 <dixons6912@student.faytechcc.edu>
2026-04-05 15:34:02 -0400

    Delete P1LAB1_Shameerah_Dixon.py

--- P1LAB1_Shameerah_Dixon.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your first name: ") # notice collected information is assigned to a variable , variable name
-name = input("Enter your last name: ") # notice collected information is assigned to a variable , variable name
-# print statement below displays what the user entered
-print("Hello",name Welcome to CTI-110)
