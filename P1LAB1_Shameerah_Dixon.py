bf0268534c3c4a1fc04be92290903aca14be9022
dixons6912 <dixons6912@student.faytechcc.edu>
2026-04-05 15:12:27 -0400

    Add files via upload

---
+++ P1LAB1_Shameerah_Dixon.py
@@ -0,0 +1,4 @@
+name = input("Enter your first name: ") # notice collected information is assigned to a variable , variable name
+name = input("Enter your last name: ") # notice collected information is assigned to a variable , variable name
+# print statement below displays what the user entered
+print("Hello",name Welcome to CTI-110)
